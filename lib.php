9520a9e2effa4e909533a862b70043a03c7caa17
Benedikt Kulmann <benedikt@kulmann.biz>
2019-12-26 07:57:22 +0100

    Fix that not only single choice, but also all other subtypes of multichoice questions are pulled in.

--- lib.php
+++ lib.php
@@ -55,12 +55,6 @@ define('MOD_MILLIONAIRE_JOKERS', [
     MOD_MILLIONAIRE_JOKER_FEEDBACK
 ]);
 
-// implemented question types
-define('MOD_MILLIONAIRE_QTYPE_SINGLE_CHOICE_DB', 'multichoice');
-define('MOD_MILLIONAIRE_VALID_QTYPES_DB', [
-    MOD_MILLIONAIRE_QTYPE_SINGLE_CHOICE_DB,
-]);
-
 /**
  * Returns the information on whether the module supports a feature
  *
